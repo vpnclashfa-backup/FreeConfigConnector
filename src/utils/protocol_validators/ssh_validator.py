921d6de7f82c1de70e332a1165cce021d0d6025e
vpnclashfa backup <213138203+vpnclashfa-backup@users.noreply.github.com>
2025-07-04 18:20:04 +0330

    Update ssh_validator.py

--- src/utils/protocol_validators/ssh_validator.py
+++ src/utils/protocol_validators/ssh_validator.py
@@ -1,5 +1,5 @@
 from src.utils.protocol_validators.base_validator import BaseValidator
-from urllib.parse import urlparse, unquote
+from urllib.parse import urlparse, unquote, quote
 
 class SshValidator(BaseValidator):
     @staticmethod
@@ -10,18 +10,16 @@ class SshValidator(BaseValidator):
             parsed_url = urlparse(link)
             
             user_host_port_part = parsed_url.netloc
-            if '@' not in user_host_port_part:
-                # Can be just host:port if username is implied or not used
-                host_port = user_host_port_part
-            else:
-                user_part, host_port = user_host_port_part.split('@', 1)
-                if not user_part: return False # User should not be empty if provided
+            if not user_host_port_part: return False # Need at least host:port
 
-            if ':' not in host_port:
-                # Default SSH port 22 or assume no port
-                host = host_port
-                port = 22 # Default SSH port
-            else:
+            host_port = user_host_port_part
+            if '@' in user_host_port_part:
+                user_part, host_port = user_host_port_part.split('@', 1)
+                if not user_part: return False # Username should not be empty if provided
+            
+            host = host_port
+            port = 22 # Default SSH port
+            if ':' in host_port:
                 host, port_str = host_port.rsplit(':', 1)
                 port = int(port_str)
                 if not BaseValidator._is_valid_port(port): return False
@@ -29,7 +27,7 @@ class SshValidator(BaseValidator):
             if not (BaseValidator._is_valid_domain(host) or BaseValidator._is_valid_ipv4(host) or BaseValidator._is_valid_ipv6(host)):
                 return False
             
-            # SSH links usually don't have complex query params like other VPNs, mostly password or key in URL
+            # SSH links usually don't have complex query params for authentication, mostly password or key in URL
             # but that's less common for actual SSH clients in URL format.
             
             return True
@@ -43,6 +41,6 @@ class SshValidator(BaseValidator):
         if len(parts) > 1:
             main_part = parts[0]
             tag_part = unquote(parts[1])
-            from urllib.parse import quote
-            cleaned_link = f"{main_part}#{quote(tag_part.strip().replace(' ', '_'))}"
+            tag_part = tag_part.strip().replace(' ', '_')
+            cleaned_link = f"{main_part}#{quote(tag_part)}"
         return cleaned_link
